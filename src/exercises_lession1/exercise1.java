12332519c95d3841a06bbf7c98b357ecd3a64c6a
Dan Truong <dantruong@MacBook-Air-cua-Dan.local>
2025-09-17 16:59:57 +0700

    first commit

---
+++ src/exercises_lession1/exercise1.java
@@ -0,0 +1,8 @@
+package exercises_lession1;
+import java.util.Date;;
+public class exercise1 {
+      private static final  Date date = new Date();
+    public static void main(String[] args) {
+        System.out.println(" now is " + date);
+    }
+}
