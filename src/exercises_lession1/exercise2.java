8e1b70e1fdbedd1cde473732a0c22f8d72f4c575
Dan Truong <dantruong@MacBook-Air-cua-Dan.local>
2025-09-18 22:18:51 +0700

    second commit

---
+++ src/exercises_lession1/exercise2.java
@@ -0,0 +1,21 @@
+package exercises_lession1;
+import java.util.Scanner;;
+public class exercise2 {
+   private static final Scanner sc = new  Scanner(System.in); 
+
+public static void main(String[] args) {
+    int i = 10;
+    float f = 20.5f;
+    double d = 20.5;
+    boolean ok = true;
+    char c = 'a';
+    String s = "Hà Nội";
+    System.out.println(i);
+    System.out.println(f);
+    System.out.println(d);
+    System.out.println(ok);
+    System.out.println(c);
+    System.out.println(s);
+sc.close();
+}
+}
